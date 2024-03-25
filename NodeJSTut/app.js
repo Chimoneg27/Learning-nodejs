2eab761dd6c30edba8d49327cf7d8c629565b57e
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-03-25 23:53:09 +0200

    Module practice with NodeJS

---
+++ NodeJSTut/app.js
@@ -0,0 +1,5 @@
+const sayHello = require('./hello.js');
+
+sayHello('Marseilles');
+sayHello('Alexander');
+sayHello('Emporio');
---
+++ NodeJSTut/hello.js
@@ -0,0 +1,5 @@
+const sayHello = (name) => {
+  console.log(`Hello, ${name}! How are you?`)
+}
+
+module.exports = sayHello;
